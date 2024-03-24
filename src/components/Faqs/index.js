e0958bcc9c3385d7b44e5395d994d848ed550e01
krishnatutorial <krishnakjp963@gmail.com>
2024-03-24 06:12:57 +0000

    error

---
+++ src/components/Faqs/index.js
@@ -0,0 +1,23 @@
+// Write your code here.
+import FaqItem from '../FaqItem'
+
+import './index.css'
+
+const Faqs = props => {
+  const {faqsList} = props
+
+  return (
+    <div>
+      <div>
+        <h1>FAQs</h1>
+        <ul>
+          {faqsList.map(eachFaq => (
+            <FaqItem key={eachFaq.id} faqDetails={eachFaq} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Faqs
